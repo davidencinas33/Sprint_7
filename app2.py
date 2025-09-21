75845f2e90f1e449a85a95218d02289cdcbe2e44
davidencinas33 <104805401+davidencinas33@users.noreply.github.com>
2025-09-20 17:29:25 -0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,16 +8,16 @@ car_data = pd.read_csv('../vehicles_us.csv')
 # Título de la aplicación
 st.header('Análisis de datos de vehículos')
 
-# Crear las casillas de verificación
-build_histogram = st.checkbox('Construir un histograma para el odómetro')
-build_scatter = st.checkbox('Construir un diagrama de dispersión de precio vs. odómetro')
+# Crear los botones
+build_histogram = st.button('Construir histograma')
+build_scatter = st.button('Construir diagrama de dispersión')
 
 if build_histogram:
-    # Si la casilla del histograma está seleccionada, construirlo
+    st.write('Construir un histograma para la columna odómetro')
     fig_hist = px.histogram(car_data, x='odometer', title='Distribución del odómetro')
     st.plotly_chart(fig_hist)
 
 if build_scatter:
-    # Si la casilla del diagrama de dispersión está seleccionada, construirlo
+    st.write('Construir un diagrama de dispersión de precio vs. odómetro')
     fig_scatter = px.scatter(car_data, x='odometer', y='price', title='Precio vs. Odómetro')
     st.plotly_chart(fig_scatter)
