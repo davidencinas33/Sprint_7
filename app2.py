748c49727fe4801e3f7af2213191c012eabd4b8d
davidencinas33 <104805401+davidencinas33@users.noreply.github.com>
2025-09-20 18:16:47 -0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 # Cargar los datos desde el archivo CSV
-car_data = pd.read_csv('../vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 
 # Título de la aplicación
 st.header('Análisis de datos de vehículos')
@@ -21,3 +21,4 @@ if build_scatter:
     st.write('Construir un diagrama de dispersión de precio vs. odómetro')
     fig_scatter = px.scatter(car_data, x='odometer', y='price', title='Precio vs. Odómetro')
     st.plotly_chart(fig_scatter)
+
