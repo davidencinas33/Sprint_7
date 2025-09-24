c97b8a17a02ad03f3643e1c4622c81bb68ad9078
davidencinas33 <104805401+davidencinas33@users.noreply.github.com>
2025-09-23 18:35:31 -0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,13 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Cargar los datos desde el archivo CSV
 car_data = pd.read_csv('vehicles_us.csv')
-
-# Título de la aplicación
 st.header('Análisis de datos de vehículos')
-
-# Crear los botones
 build_histogram = st.button('Construir histograma')
 build_scatter = st.button('Construir diagrama de dispersión')
 
@@ -21,4 +16,5 @@ if build_scatter:
     st.write('Construir un diagrama de dispersión de precio vs. odómetro')
     fig_scatter = px.scatter(car_data, x='odometer', y='price', title='Precio vs. Odómetro')
     st.plotly_chart(fig_scatter)
+
 
